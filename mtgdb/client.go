5a3ee5b101b3502881382759ad5402a37ebb2f48
Seth Curry <seth@scurry.io>
2024-01-17 14:55:11 -0600

    Adding some documentation

--- mtgdb/client.go
+++ mtgdb/client.go
@@ -11,6 +11,8 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// Open opens a new connection to the database.  Only "sqlite3" is currently
+// supported.
 func Open(driverName, dataSourceName string) (*Client, error) {
 	conn, err := sqlx.Open(driverName, dataSourceName)
 	if err != nil {
@@ -52,6 +54,12 @@ type Client struct {
 	*baseClient
 }
 
+// MigrateSchema attempts to migrate the database schema to the latest version.
+// This is only done with "CREATE IF NOT EXISTS" queries, so it will not update
+// the schema if it has already been created.
+//
+// You should pin to a release if you want to use a consistent schema with consistent data.
+// If you are okay with wiping the database and re-loading data into it, you can be more flexible.
 func (c *Client) MigrateSchema(ctx context.Context) error {
 	_, err := c.conn.ExecContext(ctx, string(schema))
 	if err != nil {
@@ -61,22 +69,30 @@ func (c *Client) MigrateSchema(ctx context.Context) error {
 	return nil
 }
 
+// Artists returns a client for interfacing with Artist objects.
 func (c *Client) Artists() *ArtistClient {
 	return newArtistClient(c.baseClient)
 }
 
+// Sets returns a client for interfacing with Set objects.
 func (c *Client) Sets() *SetClient {
 	return newSetClient(c.baseClient)
 }
 
+// Cards returns a client for interfacing with Card objects.
 func (c *Client) Cards() *CardClient {
 	return newCardClient(c.baseClient)
 }
 
+// Rulings returns a client for interfacing with Ruling objects.
 func (c *Client) Rulings() *RulingClient {
 	return newRulingClient(c.baseClient)
 }
 
+// CardFaces returns a client for interfacing with CardFaces.
+//
+// Note that Card objects lack fields like oracle text and mana cost.
+// You can find those items here on CardFaces, which are linked to a Card.
 func (c *Client) CardFaces() *CardFaceClient {
 	return newCardFaceClient(c.baseClient)
 }
