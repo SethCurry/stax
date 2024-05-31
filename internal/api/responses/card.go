d28156c0356bb64188470e396238f2f1c6b42db1
Seth Curry <seth@scurry.io>
2024-05-31 14:53:13 -0500

    Adding more fields to card face output

--- internal/api/responses/card.go
+++ internal/api/responses/card.go
@@ -12,7 +12,17 @@ type Card struct {
 }
 
 type CardFace struct {
-	OracleText string `json:"oracle_text"`
+	Name       string  `json:"name"`
+	OracleText string  `json:"oracle_text"`
+	FlavorText string  `json:"flavor_text"`
+	Language   string  `json:"language"`
+	CMC        float32 `json:"cmc"`
+	Power      string  `json:"power"`
+	Toughness  string  `json:"toughness"`
+	Loyalty    string  `json:"loyalty"`
+	ManaCost   string  `json:"mana_cost"`
+	TypeLine   string  `json:"type_line"`
+	Colors     string  `json:"colors"`
 }
 
 type CardSearch struct {
@@ -26,7 +36,17 @@ func CardFromDB(crd *oracledb.Card) Card {
 		OracleID: crd.OracleID,
 		Faces: common.Map(crd.Edges.Faces, func(f *oracledb.CardFace) CardFace {
 			return CardFace{
+				Name:       f.Name,
 				OracleText: f.OracleText,
+				FlavorText: f.FlavorText,
+				Language:   f.Language,
+				CMC:        f.Cmc,
+				Power:      f.Power,
+				Toughness:  f.Toughness,
+				Loyalty:    f.Loyalty,
+				ManaCost:   f.ManaCost,
+				TypeLine:   f.TypeLine,
+				Colors:     f.Colors,
 			}
 		}),
 	}
