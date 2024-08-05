6367a98153ecc6a0c11ea8e3815bd0c687d1fa42
Seth Curry <seth@scurry.io>
2024-08-05 10:32:50 -0500

    Saving progress

--- internal/ql/parser.go
+++ internal/ql/parser.go
@@ -1,6 +1,26 @@
 package ql
 
-import "github.com/SethCurry/stax/internal/bones/predicate"
+import (
+	"fmt"
+
+	"github.com/SethCurry/stax/internal/bones/card"
+	"github.com/SethCurry/stax/internal/bones/predicate"
+)
+
+type Operator string
+
+const (
+	EQ Operator = "="
+	NE Operator = "!="
+	GT Operator = ">"
+	GE Operator = ">="
+	LT Operator = "<"
+	LE Operator = "<="
+)
+
+type filterType func(Operator, string) (Leaf, error)
+
+type cardFilter func(string) Leaf
 
 type Leaf interface {
 	Predicate() predicate.Card
@@ -28,4 +48,27 @@ func (l *LogicNode) Left() Leaf {
 
 func (l *LogicNode) Right() Leaf {
 	return l.right
+}
+
+type basicLeaf struct {
+	predicator predicate.Card
+}
+
+func (l *basicLeaf) Predicate() predicate.Card {
+	return l.predicator
+}
+
+func cardNameSearch(op Operator, name string) (Leaf, error) {
+	switch op {
+	case EQ:
+		return &basicLeaf{
+			predicator: card.NameEQ(name),
+		}, nil
+	}
+
+	return nil, fmt.Errorf("invalid operator: %s", op)
+}
+
+var filterTypeLookupTable = map[string]filterType{
+	"name": cardNameSearch,
 }
